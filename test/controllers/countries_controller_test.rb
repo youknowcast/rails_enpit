c03b802cf88ef0575fcbbb4369e01586510b410d
youknowcast <a1902ha@aiit.ac.jp>
2019-07-03 10:38:39 +0000

    fix test failed

--- test/controllers/countries_controller_test.rb
+++ test/controllers/countries_controller_test.rb
@@ -17,7 +17,7 @@ class CountriesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create country" do
     assert_difference('Country.count') do
-      post countries_url, params: { country: {  } }
+      post countries_url, params: { country: { name: 'Singapore', capital: 'Singapore'  } }
     end
 
     assert_redirected_to country_url(Country.last)
@@ -34,7 +34,7 @@ class CountriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update country" do
-    patch country_url(@country), params: { country: {  } }
+    patch country_url(@country), params: { country: { name: 'USA', capital: 'Wasington D.C.' } }
     assert_redirected_to country_url(@country)
   end
 
