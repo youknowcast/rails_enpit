25746ea78c0f68e8021694358e1ff727e214743c
youknowcast <a1902ha@aiit.ac.jp>
2019-07-03 10:29:52 +0000

    add country pages.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_102723) do
+ActiveRecord::Schema.define(version: 2019_07_03_101508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2019_06_28_102723) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.string "capital"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
